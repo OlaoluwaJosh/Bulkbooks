5153a126df90725792ba992136647935f0d2d7f6
toshiba <toshiba@DESKTOP-BKB766U>
2026-09-14 16:06:50 +0100

    Add project files.

---
+++ bulkbooksweb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using bulkbooksweb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace bulkbooksweb.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {           
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
